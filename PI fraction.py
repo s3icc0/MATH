eb4a59583f2860b0f4fd7b58ff8bb41837b5df0d
s3icc0 <ondrej.sisler@gmail.com>
2017-04-04 08:10:48 +0200

    new

--- PI fraction.py
+++ PI fraction.py
@@ -1,10 +1,28 @@
 import math
+from decimal import Decimal as D
 
-pi120 = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647
 
-top = 1
-bot = 1
+# 92653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647
+pinr = 3.1
+dp = 4
 
-def calc():
-    res = top / bot
-    if res
+nomi = 1
+dnom = 1
+
+
+def calc(a, b, d):
+    res = D(a / b)
+    return round(res, d)
+
+def main():
+    global nomi, dnom, dp
+    if calc(nomi, dnom, dp) < pinr:
+        nomi += 1
+        main()
+    elif calc(nomi, dnom, dp) > pinr:
+        dnom += 1
+        main()
+    else:
+        print('{0}/{1}'.format(nomi, dnom))
+
+main()
